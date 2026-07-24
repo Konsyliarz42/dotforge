6500ef98722847f38ace4cd944ae25367479bcee
Tomasz Kordiak <t.kordiak@satrev.space>
2026-07-25 00:23:40 +0200

    fix

--- lib/utils.sh
+++ lib/utils.sh
@@ -3,30 +3,37 @@
 get_or_null() {
     local key="$1"
     local file="$2"
-    local -n result="$3"
+    local result_name="$3"
+    local -n result="$result_name"
 
     if [[ "$key" == *"[]" ]]; then
+        result=()
         mapfile -t result < <(yq -r "$key" "$file")
     else
-        result=("$(yq -r "$key // \"\"" "$file")")
+        result="$(yq -r "$key // \"\"" "$file")"
     fi
 }
 
 get_config() {
     local key="$1"
-    local -n result="$2"
-
-    result=()
+    local result_name="$2"
+    local -n result="$result_name"
 
     if [[ -n "$CONFIG_FILE" ]]; then
-        get_or_null "$key" "$CONFIG_FILE" result
+        get_or_null "$key" "$CONFIG_FILE" "$result_name"
     fi
 
     if [[ "$key" == *"[]" ]]; then
+        result=()
+
         if ((${#result[@]} == 0)); then
-            get_or_null "$key" "$DEFAULT_CONFIG_FILE" result
+            get_or_null "$key" "$DEFAULT_CONFIG_FILE" "$result_name"
         fi
-    elif [[ -z "${result[0]}" ]]; then
-        get_or_null "$key" "$DEFAULT_CONFIG_FILE" result
+    else
+        result=""
+
+        if [[ -z "$result" ]]; then
+            get_or_null "$key" "$DEFAULT_CONFIG_FILE" "$result_name"
+        fi
     fi
 }
