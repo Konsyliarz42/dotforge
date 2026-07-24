03b5ccedbfc73d044fde24d4b82810772ff34611
Tomasz Kordiak <t.kordiak@satrev.space>
2026-07-25 00:06:58 +0200

    fix

--- lib/utils.sh
+++ lib/utils.sh
@@ -16,12 +16,17 @@ get_config() {
     local key="$1"
     local -n result="$2"
 
+    result=()
+
     if [[ -n "$CONFIG_FILE" ]]; then
         get_or_null "$key" "$CONFIG_FILE" result
     fi
 
-    if [[ -z "$value" ]]; then
+    if [[ "$key" == *"[]" ]]; then
+        if ((${#result[@]} == 0)); then
+            get_or_null "$key" "$DEFAULT_CONFIG_FILE" result
+        fi
+    elif [[ -z "${result[0]}" ]]; then
         get_or_null "$key" "$DEFAULT_CONFIG_FILE" result
     fi
-
 }
